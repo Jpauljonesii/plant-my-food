87afdf9cc441b6623b4ec8a20289463c9095c626
John P. Jones II <jpauljonesii@gmail.com>
2024-03-02 15:20:25 -0600

    init setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function footer() {
+  return (
+    <footer> &copy; Plant My Food 2024 </footer>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <header>
+      <h1>Plant My Food</h1>
+     </header>
+  )
+}
